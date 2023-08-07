3cc3f5a50d4c6c1293f683fdeb527ec26f349f25
gha3mi <36126354+gha3mi@users.noreply.github.com>
2023-08-07 09:11:16 +0000

    Deploying to gh-pages from @ gha3mi/forpca@f53e5a793f14bc9f4e4861210081879173cfce58 🚀

---
+++ src/test1.f90
@@ -0,0 +1,43 @@
+program test1
+
+   use kinds
+   use forpca, only: tpca
+
+   implicit none
+
+   real(rk), dimension(:,:), allocatable :: matrix
+   real(rk), dimension(:,:), allocatable :: matrix_app
+   real(rk), dimension(:,:), allocatable :: coeff
+   real(rk), dimension(:,:), allocatable :: score
+   real(rk), dimension(:),   allocatable :: latent
+   real(rk), dimension(:),   allocatable :: explained
+   type(tpca)                            :: p
+
+   allocate(matrix(5,2))
+   matrix(1, 1) = 0.123_rk
+   matrix(1, 2) = 0.456_rk
+   matrix(2, 1) = 0.789_rk
+   matrix(2, 2) = 0.234_rk
+   matrix(3, 1) = 0.567_rk
+   matrix(3, 2) = 0.890_rk
+   matrix(4, 1) = 0.123_rk
+   matrix(4, 2) = 0.678_rk
+   matrix(5, 1) = 0.901_rk
+   matrix(5, 2) = 0.345_rk
+
+   call p%pca(matrix, 2, 'svd', coeff, score, latent, explained, matrix_app)
+   print*, p%matrix_app(1, 1)
+   print*, p%matrix_app(1, 2)
+   print*, p%matrix_app(2, 1)
+   print*, p%matrix_app(2, 2)
+   print*, p%matrix_app(3, 1)
+   print*, p%matrix_app(3, 2)
+   print*, p%matrix_app(4, 1)
+   print*, p%matrix_app(4, 2)
+   print*, p%matrix_app(5, 1)
+   print*, p%matrix_app(5, 2)
+   call p%dlloc()
+
+   deallocate(matrix, matrix_app, coeff, score, latent, explained)
+
+end program test1
